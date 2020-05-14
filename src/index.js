5b5b065d5ad6e58cfef5afe4b744541b4d2d3df5
Beatriz <beatrizrezener@gmail.com>
2020-05-14 09:03:45 -0300

    Get shopping cart

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
 function getShoppingCart(ids, productsList) {
+	const cartList = productsList.filter(product => ids.indexOf(product.id) > -1);
+	const products = cartList.map(product => {
+		return { 
+			name: product.name,
+			category: product.category
+		};
+	});
+	const categories = [...new Set(cartList.map(product => product.category))];
+	const promotion = promotions[categories.length-1];
+	const totalRegularPrice = cartList.reduce((acc, current) => acc + current.regularPrice, 0);
+	const totalPrice = (cartList.reduce((acc, current) => {
+		let price = current.regularPrice;
+		current.promotions.forEach(p => {
+			if (p.looks.indexOf(promotion) > -1) {
+				price = p.price;
+			}
+		});
+		return acc + price;
+
+	}, 0)).toFixed(2);
+	const discountValue = (totalRegularPrice - totalPrice).toFixed(2);
+	const discount = `${((discountValue * 100) / totalRegularPrice).toFixed(2)}%`;
+
+	return {
+		products,
+		promotion,
+		totalPrice,
+		discountValue,
+		discount,
+	};
 }
 
 module.exports = { getShoppingCart };
