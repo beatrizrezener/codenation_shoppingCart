7fc72198bfd272bf92db0c6700fb56421fcd3bb4
Beatriz Rezener <beatrizrezener@gmail.com>
2020-05-14 20:55:20 -0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+const percentFormatter =  new Intl.NumberFormat('en-IN', { style: 'percent', maximumFractionDigits: 2 });
 
 function getShoppingCart(ids, productsList) {
-	const cartList = productsList.filter(product => ids.indexOf(product.id) > -1);
-	const products = cartList.map(product => {
-		return { 
-			name: product.name,
-			category: product.category
-		};
-	});
-	const categories = [...new Set(cartList.map(product => product.category))];
-	const promotion = promotions[categories.length-1];
-	const totalRegularPrice = cartList.reduce((acc, current) => acc + current.regularPrice, 0);
-	const totalPrice = (cartList.reduce((acc, current) => {
-		let price = current.regularPrice;
-		current.promotions.forEach(p => {
-			if (p.looks.indexOf(promotion) > -1) {
-				price = p.price;
-			}
-		});
-		return acc + price;
-
-	}, 0)).toFixed(2);
+	const cartList = getCartList(productsList, ids);
+	const products = getProducts(cartList);
+	const promotion = getPromotion(cartList);
+	const totalRegularPrice = getTotalRegularPrice(cartList);
+	const totalPrice = getTotalPrice(cartList, promotion);
 	const discountValue = (totalRegularPrice - totalPrice).toFixed(2);
-	const discount = `${((discountValue * 100) / totalRegularPrice).toFixed(2)}%`;
+	const discount = percentFormatter.format((discountValue / totalRegularPrice));
 
 	return {
 		products,
@@ -31,6 +17,44 @@ function getShoppingCart(ids, productsList) {
 		discountValue,
 		discount,
 	};
+}
+
+function getCartList(productsList, ids) {
+	return productsList.filter(product => ids.indexOf(product.id) > -1);
+}
+
+function getProducts(cartList) {
+	return cartList.map(product => {
+		return {
+			name: product.name,
+			category: product.category
+		};
+	});
+}
+
+function getPrice(current, promotion) {
+	let price = current.regularPrice;
+	current.promotions.forEach(p => {
+		if (p.looks.indexOf(promotion) > -1) {
+			price = p.price;
+		}
+	});
+	return price;
+}
+function getPromotion(cartList) {
+	const categories = new Set(cartList.map(product => product.category));
+	const promotion = promotions[categories.size - 1];
+	return promotion;
+}
+
+function getTotalRegularPrice(cartList) {
+	return cartList.reduce((acc, current) => acc + current.regularPrice, 0);
+}
+
+function getTotalPrice(cartList, promotion) {
+	return (cartList.reduce((acc, current) => {
+		return acc + getPrice(current, promotion);
+	}, 0)).toFixed(2);
 }
 
 module.exports = { getShoppingCart };
